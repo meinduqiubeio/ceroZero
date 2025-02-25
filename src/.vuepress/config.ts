0f17830c5bd4c549c091d0b13a1dea7259dddf09
lwh <lwh7749@outlook.com>
2025-02-25 19:18:00 +0100

    fron trae

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,21 +1,32 @@
 import { defineUserConfig } from "vuepress";
-
-
+import { googleAnalyticsPlugin } from '@vuepress/plugin-google-analytics'
+import googleAdSensePlugin from 'vuepress-plugin-google-adsense2';
 import theme from "./theme.js";
 
 export default defineUserConfig({
+
+  plugins: [
+    // 添加你需要的插件
+    googleAdSensePlugin({
+      id: 'ca-pub-2072750546202515',
+    }),
+    googleAnalyticsPlugin({
+      // 配置项
+      id: 'G-G63MSQ1CCE',
+    })
+  ],
   base: "/",
 
   locales: {
     "/": {
       lang: "en-US",
-      title: "Blog Demo",
-      description: "A blog demo for vuepress-theme-hope",
+      //title: "Blog Demo",
+      description: "Share your life in Spain",
     },
     "/zh/": {
       lang: "zh-CN",
-      title: "博客演示",
-      description: "vuepress-theme-hope 的博客演示",
+      //title: "cero zero",
+      description: "西班牙生活、科技 分享",
     },
   },
 
